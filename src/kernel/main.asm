3a13b012a625b32e71a746aaa8a4a9381461a4f8
Dreamfyre19 <ianduong19@icloud.com>
2025-03-19 08:26:29 -0500

    Add files via upload

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,10 +1,17 @@
-org 0x7C00
+org 0x0
 bits 16
+
 
 %define ENDL 0x0D, 0x0A
 
+
 start:
-    jmp main
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
 
 puts:
     push si
@@ -12,12 +19,12 @@ puts:
     push bx
 
 .loop:
-    lodsb
-    or al, al
+    lodsb         
+    or al, al   
     jz .done
 
-    mov ah, 0x0E
-    mov bh, 0
+    mov ah, 0x0E 
+    mov bh, 0 
     int 0x10
 
     jmp .loop
@@ -25,26 +32,7 @@ puts:
 .done:
     pop bx
     pop ax
-    pop si
+    pop si    
     ret
 
-main:
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-
-    mov ss, ax
-    mov sp, 0x7C00
-
-    mov si, msg_hello
-    call puts
-
-    hlt
-    
-.halt:
-    jmp .halt
-
-msg_hello: db 'Welocome to ThanOS!', ENDL, 0
-
-times 510-($-$$) db 0
-dw 0AA55h
+msg_hello: db 'Welcome to the ThanOS world', ENDL, 0
